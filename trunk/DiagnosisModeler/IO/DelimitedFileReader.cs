a01e9c71492372e4e53ebe9735a55944c8e34bbb
blfrie <blfrie@698b2a66-aba8-11de-b6f4-e1be98fab775>
2009-09-29 01:02:31 +0000

    Initial commit

---
+++ trunk/DiagnosisModeler/IO/DelimitedFileReader.cs
@@ -0,0 +1,10 @@
+namespace DiagnosisModeler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class DelimitedFileReader
+    {
+    }
+}
---
+++ trunk/DiagnosisModeler/MainForm.Designer.cs
@@ -0,0 +1,136 @@
+namespace DiagnosisModeler
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openModelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveModelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newModelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveModelToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(933, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "mnuMain";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newModelToolStripMenuItem,
+            this.openModelToolStripMenuItem,
+            this.saveModelToolStripMenuItem,
+            this.saveModelToolStripMenuItem1,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openModelToolStripMenuItem
+            // 
+            this.openModelToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("openModelToolStripMenuItem.Image")));
+            this.openModelToolStripMenuItem.Name = "openModelToolStripMenuItem";
+            this.openModelToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openModelToolStripMenuItem.Text = "&Open...";
+            // 
+            // saveModelToolStripMenuItem
+            // 
+            this.saveModelToolStripMenuItem.Enabled = false;
+            this.saveModelToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("saveModelToolStripMenuItem.Image")));
+            this.saveModelToolStripMenuItem.Name = "saveModelToolStripMenuItem";
+            this.saveModelToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveModelToolStripMenuItem.Text = "&Save";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
+            // newModelToolStripMenuItem
+            // 
+            this.newModelToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("newModelToolStripMenuItem.Image")));
+            this.newModelToolStripMenuItem.Name = "newModelToolStripMenuItem";
+            this.newModelToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newModelToolStripMenuItem.Text = "New...";
+            // 
+            // saveModelToolStripMenuItem1
+            // 
+            this.saveModelToolStripMenuItem1.Enabled = false;
+            this.saveModelToolStripMenuItem1.Name = "saveModelToolStripMenuItem1";
+            this.saveModelToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.saveModelToolStripMenuItem1.Text = "Save As...";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 493);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmMain";
+            this.Text = "Diagnosis Modeler";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openModelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveModelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newModelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveModelToolStripMenuItem1;
+    }
+}
+
