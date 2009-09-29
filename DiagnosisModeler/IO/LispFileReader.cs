ddf173b4a53adfbb0189bdcccb0f1f88df12b986
blfrie <blfrie@698b2a66-aba8-11de-b6f4-e1be98fab775>
2009-09-29 01:02:31 +0000

    Initial commit

---
+++ DiagnosisModeler/IO/LispFileReader.cs
@@ -0,0 +1,18 @@
+namespace DiagnosisModeler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.IO;
+
+    /// <summary>
+    /// This class reads in a LISP file and tries to turn the contents
+    /// into a data model
+    /// </summary>
+    public class LispFileReader : TextReader
+    {
+        public LispFileReader()
+        {
+        }
+    }
+}
